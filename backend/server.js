f21a79cf17446f2298e0524ac80269b439d6b817
Shailendra Pathak <pathakharsh813@gmail.com>
2025-04-22 14:21:25 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import app from "./src/app.js";
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,17 @@
+import { GoogleGenAI } from '@google/genai';
+import config from '../config/config';
+
+const ai = new GoogleGenAI({ apiKey: config.GOOGLE_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "create a function in js to add two numbers",
+    config : {
+        systemInstruction: "you are a experienced software engineer, you are expert in MERN stack, you are developing application from 7 years. you write code in such as way that no error will be there, you are wroting a code which is easy to understand and maintainable",
+    }
+  });
+  console.log(response.text);
+}
+
+await main();
