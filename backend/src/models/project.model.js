bb8ec0f8ade82a454be277de08b55e4ba3d5e6e9
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-25 10:34:58 +0530

    building project model

---
+++ backend/src/models/project.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Project name is required'],
+        trim: true,
+        unique: true,
+    }
+});
